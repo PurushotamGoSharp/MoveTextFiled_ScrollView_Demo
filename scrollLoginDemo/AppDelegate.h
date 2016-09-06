0b918b6f3f687581e9cf31667e4bcb45ab4dd991
senthilnathan Karuppaiah <senthilnathank@vmokshagroup.com>
2016-09-06 10:23:09 +0530

    Initial Commit

---
+++ scrollLoginDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  scrollLoginDemo
+//
+//  Created by Jenkins on 06/09/16.
+//  Copyright © 2016 srinivas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ scrollLoginDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  scrollLoginDemo
+//
+//  Created by Jenkins on 06/09/16.
+//  Copyright © 2016 srinivas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
